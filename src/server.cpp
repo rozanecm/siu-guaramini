9cba7d86521424af92096fac47d105df5789f83b
Matias Rozanec <rozanecm@gmail.com>
2017-04-27 11:56:25 -0300

    create server class. Move file processing methods here, because server stores file data

--- src/server.cpp
+++ src/server.cpp
@@ -1,82 +1,62 @@
-#include <iostream>
-#include <map>
-#include <fstream>
-#include "serverUser.h"
 #include "serverSubject.h"
+#include "serverUser.h"
+#include <fstream>
+#include <map>
+#include <iostream>
+#include "server.h"
 #include "serverTeacher.h"
 #include "serverStudent.h"
 
-void processUsersFile(std::string filePath, std::map<int, User*> &users);
 
-void processSubjectsFile(std::string filepath,
-                         std::map<std::pair<int, int>, Subject *> &subjects);
 
-int main(int argc, char *argv[]) {
-    /* check num. of arguments received */
-//    if (argc != 4){
-//        std::cerr<<"Invalid number of arguments"<<std::endl;
-//        return 0;
-//    }
 
-    /* retrieve information from arguments*/
-    std::string port = argv[1];
-    std::string usersFilepath = argv[1];
-    std::string subjectsFilepath = argv[2];
 
-    /* build structures that will hold file data */
-    std::map<int, User*> users;
-    std::map<std::pair<int, int>, Subject*> subjects;
-
-    /* store file information */
-    processUsersFile(usersFilepath, users);
-    processSubjectsFile(subjectsFilepath, subjects);
-    return 0;
-}
-
-void processSubjectsFile(std::string filepath,
-std::map<std::pair<int, int>, Subject *> &subjects) {
+void server::processSubjectsFile(std::string filepath) {
     std::ifstream subjectsFile;
-    try {
-        subjectsFile.open(filepath);
-    }catch (std::ios_base::failure e){
-        std::cerr<<"Error opening error file"<<std::endl;
-    }
+    subjectsFile.open(filepath);
     subjectsFile.seekg(0);
+
     while (subjectsFile.peek() != EOF) {
         std::string description, subjectID, courseID, teacherID, vacantes;
 
-        std::getline(subjectsFile, subjectID, '\t');
-        std::getline(subjectsFile, courseID, '\t');
-        std::getline(subjectsFile, description, '\t');
+        getline(subjectsFile, subjectID, '\t');
+        getline(subjectsFile, courseID, '\t');
+        getline(subjectsFile, description, '\t');
 
-        std::getline(subjectsFile, teacherID, '\t');
-        std::getline(subjectsFile, vacantes);
+        getline(subjectsFile, teacherID, '\t');
+        getline(subjectsFile, vacantes);
 
-        subjects.insert(std::make_pair(std::make_pair(std::stoi(subjectID), std::stoi(courseID)),
-                                       new Subject(std::stoi(teacherID),
-                                                   std::stoi(vacantes),
-                                                   description)));
+        subjects.emplace(std::make_pair(stoi(subjectID), stoi(courseID)),
+                         Subject(stoi(subjectID), stoi(courseID),
+                                 stoi(teacherID), stoi(vacantes), description));
     }
     subjectsFile.close();
 }
 
-void processUsersFile(std::string filePath, std::map<int, User*> &users) {
+void server::processUsersFile(std::string filePath) {
     std::ifstream usersFile;
-    try {
-        usersFile.open(filePath);
-    }catch (std::ios_base::failure e){
-        std::cerr<<"Error opening error file"<<std::endl;
-    }
+    usersFile.open(filePath);
     usersFile.seekg(0);
+
     while (!usersFile.eof()) {
         std::string userType, name;
         int id;
         usersFile>>userType>>id>>name;
         if (userType == "docente"){
-            users.insert(std::make_pair(id, new Teacher(name)));
+            teachers.emplace(std::piecewise_construct, std::make_tuple(id),
+                             std::make_tuple(id, name));
+//            teachers.emplace(id, id, name);
+//            teachers.emplace(id, Teacher(id, name));
+//            teachers.insert(std::make_pair(id, new Teacher(name)));
         }else{
-            users.insert(std::make_pair(id, new Student(name)));
+//            students[id] = Student(id, name);
+            students.emplace(std::piecewise_construct, std::make_tuple(id),
+                             std::make_tuple(id, name));
+//            students.emplace(id, id, name);
+//            students.emplace(id, Student(id, name));
+//            students.insert(std::make_pair(id, new Student(name)));
         }
     }
+
     usersFile.close();
 }
